7679c631c742882181999bfab3ff47b338b7c045
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2018-11-25 18:54:15 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        {{title}}
+        <Character
+        v-for="(id, index) in initial_ids" 
+        :id="id"
+        :key="index" 
+        />  
+    </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Generate Your team',
+            initial_ids: [1, 13, 14]
+        }
+    },
+    components: {
+        Character
+    }
+}
+</script>
